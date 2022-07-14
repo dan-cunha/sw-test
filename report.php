eb99b75c5e48243f171813062c68dc25ce8cb4c0
Dan da Cunha <dancunha@gmail.com>
2022-07-14 13:12:35 +0200

    HTML stubbed out for results

--- report.php
+++ report.php
@@ -11,15 +11,62 @@ class Report {
   }
 
   function getResults( $filter = ''){
-    $query = 'SELECT orderid, comments, shipdate_expected FROM `sweetwater_test`';
+    $query = 'SELECT orderid, comments, shipdate_expected FROM `sweetwater_test` ';
+
+    // can be smarter about the WHERE/OR/AND stuff in future iterations
+    switch ($filter) {
+      case 'candy':
+        $query .= ' WHERE comments LIKE "%candy%"';
+        break;
+      break;
+    }
 
     $res = $this->db->query($query);
-    return $res->fetch_all();
+    return $res->fetch_all(MYSQLI_ASSOC);
+  }
+
+  function getCandyResults(){
+    return $this->getResults('candy');
+  }
+
+  function getCallBackResults(){
+    return $this->getResults('callback');
   }
 }
 
-
 $report = new Report($mysqli);
+$candyResults = $report->getCandyResults();
+$callbackResults = $report->getCallBackResults();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DDC - Sweetwater Code Test</title>
+  </head>
+  <body>
+    <h1>Sweetwater Code Test</h1>
 
-echo '<pre>';
-print_r($report->getResults());
+    <h3>Candy Results</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>Order ID</th>
+          <th>Comments</th>
+          <th>Ship Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($candyResults as $result) : ?>
+          <tr>
+            <td><?php echo $result['orderid']; ?></td>
+            <td><?php echo $result['comments']; ?></td>
+            <td><?php echo $result['shipdate_expected']; ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </body>
+</html>
