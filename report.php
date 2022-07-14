6e730de84800d02392b790f20d9262f828481fcd
Dan da Cunha <dancunha@gmail.com>
2022-07-14 13:02:24 +0200

    start building a reusable base class

--- report.php
+++ report.php
@@ -5,8 +5,21 @@ require_once('./config.php');
 // connect to the data
 $mysqli = new mysqli($database['host'], $database['username'], $database['password'], $database['database']);
 
-$res = $mysqli->query("SELECT * FROM `sweetwater_test`");
+class Report {
+  function __construct($mysqli) {
+    $this->db = $mysqli;
+  }
 
-foreach ($res as $row) {
-  echo $row['orderid'] . ' - ' . $row['comments'] . '<hr>';
+  function getResults( $filter = ''){
+    $query = 'SELECT orderid, comments, shipdate_expected FROM `sweetwater_test`';
+
+    $res = $this->db->query($query);
+    return $res->fetch_all();
+  }
 }
+
+
+$report = new Report($mysqli);
+
+echo '<pre>';
+print_r($report->getResults());
